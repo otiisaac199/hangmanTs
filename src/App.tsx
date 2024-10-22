53884bb571077c395a8839e95514d29103f86cf7
otiisaac199 <otichinazaisaac19@gmail.com>
2024-10-22 18:42:32 +0100

    feat: added some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
+
 const App = () => {
-  return <div style={{ fontSize: "45px" }}>App</div>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2rem",
+        margin: "0 auto",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose or Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 };
 
 export default App;
